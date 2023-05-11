6b380db26216c5ec5203175ad1f6fbafbbf698be
YiDaoJ <jcxsusan@gmail.com>
2023-05-11 13:56:14 +0200

    add typescript to server

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.API_PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + Typescript server!sd');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+// Ref: https://blog.logrocket.com/how-to-set-up-node-typescript-express/
